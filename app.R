3e95a7b6c66806231c191a6bb0ef2b08567c1871
David Bray <bray@bu.edu>
2020-08-25 16:57:19 -0400

    Added recruitment heatmap and updated motif grid

--- app.R
+++ app.R
@@ -9,11 +9,19 @@ options(digits=22)
 library(ggplot2)
 library(ggseqlogo)
 library(gridExtra)
+library(reshape2)
+library(heatmaply)
 
 # load shiny app libraries
 library(shiny)
 library(shinythemes)
 library(DT)
+
+# load Bioconductor packages
+library(BiocManager)
+options(repos = BiocManager::repositories())
+library(universalmotif)
+library(TFBSTools)
 
 # scale transformation function for rounding axes
 scaleFUN <- function(x) sprintf("%.1f", x)
@@ -26,10 +34,10 @@ zscore_to_SNV_matrix <- function(PBM, PBM_col, target_seq_col, target_seq) {
   SNV <- matrix(nrow=4, ncol=nchar(as.character(target_seq)))
   
   # collect z-score values for each position in the matrix
-  for (i in 1:nrow(SNV)) {
-    for (j in 1:ncol(SNV)) {
+  for (i in 1:base::nrow(SNV)) {
+    for (j in 1:base::ncol(SNV)) {
       # construct the current sequence of interest using the current SNV
-      SNV_seq <- paste(substr(target_seq, 1, j-1), nucs[i], substr(target_seq, j+1, ncol(SNV)), sep="")
+      SNV_seq <- paste(substr(target_seq, 1, j-1), nucs[i], substr(target_seq, j+1, base::ncol(SNV)), sep="")
       stopifnot(SNV_seq %in% as.character(PBM[, target_seq_col]))
       
       # collect the z-score for that given SNV probe sequence and condition
@@ -46,10 +54,10 @@ zscore_to_SNV_matrix <- function(PBM, PBM_col, target_seq_col, target_seq) {
 SNV_to_SNV_trans <- function(SNV) {
   
   # transform z-score values to reflect deviation from column-wise median
-  SNV_trans <- matrix(nrow=4, ncol=ncol(SNV))
+  SNV_trans <- matrix(nrow=4, ncol=base::ncol(SNV))
   rownames(SNV_trans) <- c("A", "C", "G", "T")
-  for (i in 1:nrow(SNV_trans)) {
-    for (j in 1:ncol(SNV_trans)) {
+  for (i in 1:base::nrow(SNV_trans)) {
+    for (j in 1:base::ncol(SNV_trans)) {
       SNV_trans[i, j] <- SNV[i, j] - median(SNV[,j])
     }
   }
@@ -57,6 +65,41 @@ SNV_to_SNV_trans <- function(SNV) {
   # return the PWM matrix to be plotted as a sequence logo
   return(SNV_trans)
 }
+
+# parameter and function to transfrom a SNV energy matrix to PWM
+beta <- 2.5
+SNV_to_PWM <- function(SNV, beta) {
+  
+  # transform using beta parameter
+  SNV_pwm <- exp(beta * SNV)
+  
+  # compute probabilities for PWM using the column-wise totals
+  PWM <- matrix(nrow=4, ncol=base::ncol(SNV_pwm))
+  for (i in 1:base::nrow(PWM)) {
+    for (j in 1:base::ncol(PWM)) {
+      PWM[i, j] <- SNV_pwm[i, j]/sum(SNV_pwm[,j])
+    }
+  }
+  rownames(PWM) <- c("A", "C", "G", "T")
+  
+  # return the PWM to parent function
+  return(PWM)
+  
+}
+
+# read in the hTF meme motif collection
+hTF_meme_file <- "./Data/JASPAR2018_hTF_only.meme"
+hTF_ref_motifs <- read_meme(hTF_meme_file)
+
+# convert the universalmotif object to a standard list that is easier to manipulate/reference
+hTF_motifs <- list()
+for (i in 1:length(hTF_ref_motifs)) {
+  temp_list <- list(hTF_ref_motifs[[i]]@motif)
+  names(temp_list) <- hTF_ref_motifs[[i]]@name
+  hTF_motifs <- c(hTF_motifs, temp_list)
+}
+hTF_motifs_names <- names(hTF_motifs)
+rm(hTF_ref_motifs)
 
 # declare version-independent annotation columns
 annot_cols <- c("TF_name", "ID", "family", "TF_class", "cons_seq")
@@ -121,14 +164,15 @@ ui <- navbarPage("hTF array analysis (v1.0.0)",
                uiOutput("selectMotifExp"),
 
                # user-selected TF group to compare
-               uiOutput("selectMotifTF")
+               uiOutput("selectMotifTF"),
                
-             ),
+             width = 3),
+             
              mainPanel(
                tabsetPanel(type = "tabs",
                            tabPanel("Energy logos", plotOutput("motif1", height="auto")),
-                           tabPanel("Position-weight logos"))
-             )
+                           tabPanel("Position-weight logos")),
+             width = 9)
            )
   ),
   
@@ -136,14 +180,17 @@ ui <- navbarPage("hTF array analysis (v1.0.0)",
   tabPanel("Recruitment heatmap",
            sidebarLayout(
              sidebarPanel(
-               # TODO
-             ),
+               
+               # user-selected PBM experiments to compare
+               uiOutput("selectHeatmapExp"),
+               
+             width = 4),
+
              mainPanel(
                tabsetPanel(type = "tabs",
-                           tabPanel("Tab 1"),
-                           tabPanel("Tab 2"),
-                           tabPanel("Tab 3"))
-             )
+                           tabPanel("similarity-scaled z-score", plotlyOutput("heatmap1", height="600px")),
+                           tabPanel("unscaled z-score")),
+             width = 8)
            )
   )
   
@@ -174,6 +221,12 @@ server <- function(input, output) {
   TF_choices <- reactive({
     TF_temp <- unique(df_hTF()[, "TF_name"])
     TF_temp[!is.na(TF_temp)]
+  })
+  
+  # collect motif IDs for the TFs in the current design
+  motif_IDs <- reactive({
+    motif_IDs_temp <- unique(df_hTF()[, "ID"])
+    motif_IDs_temp[!is.na(motif_IDs_temp)]
   })
   
   ### DATA TABLE CONSTRUCTION FOR EXPLORER
@@ -275,7 +328,7 @@ server <- function(input, output) {
   
   # collect a motif plot list
   motif_list <- reactive({
-    lapply(seq(nrow(TF_PBM_comb())), function(m) {
+    lapply(seq(base::nrow(TF_PBM_comb())), function(m) {
       
       # determine seed sequence for the current TF
       seed_seq <- df_hTF()[which(df_hTF()$TF_name==TF_PBM_comb()[m, "TFs"] & df_hTF()$SNV_pos_offset==0), "target_seq"]
@@ -296,18 +349,18 @@ server <- function(input, output) {
         ggtitle(paste(TF_PBM_comb()[m, "TFs"], TF_PBM_comb()[m, "PBM_exp"], round(seed_score, digits = 3), sep="\n")) +
         scale_x_continuous(expand = c(0.01,0.01)) +
         scale_y_continuous(expand = c(0.01,0.01), labels=scaleFUN) +
-        annotate('rect', xmin = 0.5, xmax = ncol(SNV_mat_trans)+0.5, ymin = my.ggp.yrange[1], ymax = 0.0, alpha = 0.75, col='white', fill='white')
+        annotate('rect', xmin = 0.5, xmax = base::ncol(SNV_mat_trans)+0.5, ymin = my.ggp.yrange[1], ymax = 0.0, alpha = 0.75, col='white', fill='white')
         
       # place a transparent window over the logo if the seed score is below threshold
       ggp <- ggp +
         if (seed_score < 1.5) {
-          annotate('rect', xmin = 0.5, xmax = ncol(SNV_mat_trans)+0.5, ymin = my.ggp.yrange[1], ymax= my.ggp.yrange[2], alpha = 0.75, col='white', fill='white')
+          annotate('rect', xmin = 0.5, xmax = base::ncol(SNV_mat_trans)+0.5, ymin = my.ggp.yrange[1], ymax= my.ggp.yrange[2], alpha = 0.75, col='white', fill='white')
         }  
       
-      # finixh plotting axis labels and borders
+      # finish plotting axis labels and borders
       ggp <- ggp +
-        annotate('rect', xmin = 0.5, xmax = ncol(SNV_mat_trans)+0.5, ymin = 0, ymax = my.ggp.yrange[2], col="lightgray", fill=NA, size=1.5) +
-        annotate('rect', xmin = 0.5, xmax = ncol(SNV_mat_trans)+0.5, ymin = my.ggp.yrange[1], ymax = 0, col="lightgray", fill=NA, size=1.5) +
+        annotate('rect', xmin = 0.5, xmax = base::ncol(SNV_mat_trans)+0.5, ymin = 0, ymax = my.ggp.yrange[2], col="#abbed1", fill=NA, size=1.5) +
+        annotate('rect', xmin = 0.5, xmax = base::ncol(SNV_mat_trans)+0.5, ymin = my.ggp.yrange[1], ymax = 0, col="#abbed1", fill=NA, size=1.5) +
         ylab(expression(paste(Delta, "z-score", sep=""))) +
         theme(axis.text.x=element_blank()) +
         theme(axis.title.x=element_blank()) +
@@ -320,6 +373,46 @@ server <- function(input, output) {
     })
   })
   
+  # modify motif plot list to include reference motifs
+  motif_list_mod <- reactive({
+    
+    # create a temp motif list to modify
+    motif_list_temp <- motif_list()
+    
+    # clone the user-selected TFs to iterate over
+    user_TFs <- input$TFs_motif
+    
+    # for each TF the user has selected for motif gridding
+    for (i in 1:length(user_TFs)) {
+      
+      # fetch ID for the current motif
+      curr_ID <- df_hTF()[which(df_hTF()$TF_name==user_TFs[i] & df_hTF()$SNV_pos_offset==0), "ID"]
+      
+      # plot the reference TF motif using its current ID
+      curr_motif <- hTF_motifs[[curr_ID]]
+      ggp <- ggseqlogo(curr_motif, seq_type="dna")
+      my.ggp.yrange <- ggplot_build(ggp)$layout$panel_params[[1]]$y.range
+      ggp <- ggp +
+        ggtitle(paste(user_TFs[i], "JASPAR 2018 CORE", curr_ID, sep='\n')) +
+        scale_x_continuous(expand = c(0.01,0.01)) +
+        scale_y_continuous(expand = c(0.01,0.01), labels=scaleFUN) +
+        annotate('rect', xmin = 0.5, xmax = base::ncol(curr_motif)+0.5, ymin = my.ggp.yrange[1], ymax = my.ggp.yrange[2], col="#added5", fill=NA, size=1.5) +
+        theme(axis.text.x=element_blank()) +
+        theme(axis.title.x=element_blank()) +
+        theme(axis.text.y=element_text(size=10, face="plain"), axis.title.y=element_text(size=10, face="plain")) +
+        theme(plot.title = element_text(size=10))
+      
+      # append the logo to motif_list_temp in appropriate spot
+      motif_list_temp <- append(motif_list_temp, list(ggp), i*length(input$PBM_exp_motif)+i-1)
+      
+    }
+    
+    # return the temp motif list to parent
+    motif_list_temp
+    
+  })
+  
+  
   # declare grid height function (150px x number of TFs)
   grid_height_fn <- eventReactive({input$PBM_exp_motif
     input$TFs_motif}, {
@@ -328,8 +421,96 @@ server <- function(input, output) {
     
   # output a motif grid
   output$motif1 <- renderPlot({
-    do.call(gridExtra::grid.arrange, c(motif_list(), ncol=length(input$PBM_exp_motif)))
+    do.call(gridExtra::grid.arrange, c(motif_list_mod(), ncol=length(input$PBM_exp_motif)+1))
   }, height=grid_height_fn)
+  
+  ### CONDITIONAL RECRUITMENT HEATMAP UI OPTIONS
+  
+  # user-specified PBM experiment group
+  output$selectHeatmapExp <- renderUI({
+    selectInput("PBM_exp_heatmap", "Choose experiments to compare", PBM_exp(), selected = PBM_exp(), multiple = T)
+  })
+  
+  ### PROCESS AND PLOT THE RECRUITMENT HEATMAP
+
+  # expand out the combinations of TFs and PBM experiments
+  TF_PBM_comb_heatmap <- eventReactive(input$PBM_exp_heatmap, {
+    motif_IDs_heatmap <- rep(motif_IDs(), each=length(input$PBM_exp_heatmap))
+    TF_PBM_comb_heatmap_temp <- expand.grid(input$PBM_exp_heatmap, TF_choices(), stringsAsFactors = F)
+    TF_PBM_comb_heatmap_temp$IDs <- motif_IDs_heatmap
+    names(TF_PBM_comb_heatmap_temp) <- c("PBM_exp", "TFs", "IDs")
+    TF_PBM_comb_heatmap_temp
+  })
+  
+  # collect a hybrid score list
+  score_list <- reactive({
+    lapply(seq(base::nrow(TF_PBM_comb_heatmap())), function(m) {
+      
+      # determine seed sequence for the current TF
+      seed_seq <- df_hTF()[which(df_hTF()$TF_name==TF_PBM_comb_heatmap()[m, "TFs"] & df_hTF()$SNV_pos_offset==0), "target_seq"]
+      
+      # determine the seed score for the current TF, PBM_exp combo
+      seed_score <- df_hTF()[which(df_hTF()$TF_name==TF_PBM_comb_heatmap()[m, "TFs"] & df_hTF()$SNV_pos_offset==0), TF_PBM_comb_heatmap()[m, "PBM_exp"]]
+      if (seed_score < 0) { seed_score <- 0 }
+      
+      # collect SNV matrix for the current combination
+      SNV_mat <- zscore_to_SNV_matrix(df_hTF()[which(df_hTF()$TF_name==TF_PBM_comb_heatmap()[m, "TFs"]), ], TF_PBM_comb_heatmap()[m, "PBM_exp"], "target_seq", seed_seq)
+      
+      # convert the SNV matrix to PWM to compare to reference motifs
+      PWM_mat <- SNV_to_PWM(SNV_mat, beta)
+      
+      # compute similarity to reference motif using the motif ID of the current entry
+      sim_score <- PWMSimilarity(PWM_mat, hTF_motifs[[TF_PBM_comb_heatmap()[m, "IDs"]]], method="Pearson")
+      if (sim_score < 0) { sim_score <- 0}
+      
+      # scale the seed z-score by the similarity score to create a hybrid
+      hybrid_score <- sim_score * seed_score
+      
+      # return the hybrid score
+      hybrid_score
+      
+    })
+  })
+  
+  # format a filtered matrix to plot the heatmap from
+  score_mat_filt <- reactive({
+    
+    # convert the score_list into a numeric vector to bind to the expanded grid
+    score_vec <- as.numeric(score_list())
+    
+    # create temp combination grid to alter
+    TF_PBM_comb_heatmap_temp <- TF_PBM_comb_heatmap()
+    
+    # bind the scores to the expanded grid and omit the motif IDs
+    TF_PBM_comb_heatmap_temp$score_vec <- score_vec
+    TF_PBM_comb_heatmap_temp <- TF_PBM_comb_heatmap_temp[, c("PBM_exp", "TFs", "score_vec")]
+    
+    # dcast the expanded grid into a TF by PBM_exp data frame
+    score_mat <- dcast(TF_PBM_comb_heatmap_temp, TFs ~ PBM_exp, value.var="score_vec")
+    score_TFs <- score_mat$TFs
+    score_mat <- score_mat[, 2:base::ncol(score_mat)]
+    
+    # reformat into a matrix
+    score_mat <- as.matrix(score_mat)
+    rownames(score_mat) <- score_TFs
+    
+    # filter the matrix based on a threshold max score for each TF
+    max_scores <- apply(score_mat, 1, max)
+    score_mat_filt_temp <- score_mat[which(max_scores > 2), ] 
+    
+    # output the filt temp matrix
+    score_mat_filt_temp
+    
+  })
+  
+  # output a heatmap
+  output$heatmap1 <- renderPlotly({
+    heatmaply(score_mat_filt(), fontsize_row = 7, fontsize_col = 7,
+              scale_fill_gradient_fun = ggplot2::scale_fill_gradient2(
+                low = "white",
+                high = "#2c3e50"
+              ))
+  })
   
 }
 
